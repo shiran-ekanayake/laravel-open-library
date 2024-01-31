26a9cff14f8ab4b98aebeac27487d19616748b53
Shiran Ekanayake <shiran.ekan@gmail.com>
2024-01-31 19:37:37 +1100

    Author search and get improvements

---
+++ src/Classes/OpenLibraryAuthor.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Sse\OpenLibrary\Classes;
+
+use Illuminate\Support\Collection;
+
+class OpenLibraryAuthor {
+
+    protected string $key;
+    protected string $name;
+    protected string $title = '';
+    protected string $bio = '';
+    protected string $personalName = '';
+    protected Collection $alternateNames;
+    protected string $birthDate = '';
+    protected string $deathDate = '';
+    protected string $period = '';
+    protected string $wikipeadiaLink = '';
+    protected Collection $links;
+    protected Collection $photos;
+    protected string $topWork = '';
+    protected int $workCount = 0;
+    protected Collection $topSubjects;
+
+    public function __construct(string $key, string $name)
+    {
+        $this->key = $key;
+        $this->name = $name;
+        $this->alternateNames = collect();
+        $this->links = collect();
+        $this->photos = collect();
+        $this->topSubjects = collect();
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function setKey(string $key): self
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getBio(): string
+    {
+        return $this->bio;
+    }
+
+    public function setBio(string $bio): self
+    {
+        $this->bio = $bio;
+        return $this;
+    }
+
+    public function getPersonalName(): string
+    {
+        return $this->personalName;
+    }
+
+    public function setPersonalName(string $personalName): self
+    {
+        $this->personalName = $personalName;
+        return $this;
+    }
+
+    public function getAlternateNames(): Collection
+    {
+        return $this->alternateNames;
+    }
+
+    public function setAlternateNames(Collection $alternateNames): self
+    {
+        $this->alternateNames = $alternateNames;
+        return $this;
+    }
+
+    public function getBirthDate(): string
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(string $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+        return $this;
+    }
+
+    public function getDeathDate(): string
+    {
+        return $this->deathDate;
+    }
+
+    public function setDeathDate(string $deathDate): self
+    {
+        $this->deathDate = $deathDate;
+        return $this;
+    }
+
+    public function getPeriod(): string
+    {
+        return $this->period;
+    }
+
+    public function setPeriod(string $period): self
+    {
+        $this->period = $period;
+        return $this;
+    }
+
+    public function getWikipeadiaLink(): string
+    {
+        return $this->wikipeadiaLink;
+    }
+
+    public function setWikipeadiaLink(string $wikipeadiaLink): self
+    {
+        $this->wikipeadiaLink = $wikipeadiaLink;
+        return $this;
+    }
+
+    public function getLinks(): Collection
+    {
+        return $this->links;
+    }
+
+    public function setLinks(Collection $links): self
+    {
+        $this->links = $links;
+        return $this;
+    }
+
+    public function getPhotos(): Collection
+    {
+        return $this->photos;
+    }
+
+    public function setPhotos(Collection $photos): self
+    {
+        $this->photos = $photos;
+        return $this;
+    }
+
+    public function getTopWork(): string
+    {
+        return $this->topWork;
+    }
+
+    public function setTopWork(string $topWork): self
+    {
+        $this->topWork = $topWork;
+        return $this;
+    }
+
+    public function getWorkCount(): int
+    {
+        return $this->workCount;
+    }
+
+    public function setWorkCount(int $workCount): self
+    {
+        $this->workCount = $workCount;
+        return $this;
+    }
+
+    public function getTopSubjects(): Collection
+    {
+        return $this->topSubjects;
+    }
+
+    public function setTopSubjects(Collection $topSubjects): self
+    {
+        $this->topSubjects = $topSubjects;
+        return $this;
+    }
+}
--- src/OpenLibrary.php
+++ src/OpenLibrary.php
@@ -6,33 +6,85 @@ use Exception;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Http;
+use Sse\OpenLibrary\Classes\OpenLibraryAuthor;
 
 class OpenLibrary
 {
     const BASE_URL = 'https://openlibrary.org/';
 
-    public function searchAuthor(string $query) : Collection {
+    /**
+     * Search for authors by a given query
+     *
+     * @param string $query
+     * @return Collection
+     */
+    public function searchAuthors(string $query) : Collection {
         $results = $this->openLibraryHttpGetRequest('search/authors.json', ['q' => $query]);
         $result = $results->object();
 
         if($result->numFound > 0) {
             return collect($result->docs)->map(function ($author) {
-                return [
-                    'key' => $author->key,
-                    'name' => $author->name,
-                ];
+                return (new OpenLibraryAuthor($author->key, $author->name))
+                ->setAlternateNames(isset($author->alternate_names) ? collect($author->alternate_names) : collect())
+                ->setBirthDate($result->birth_date ?? '')
+                ->setTopSubjects(isset($author->top_subjects) ? collect($author->top_subjects) : collect())
+                ->setTopWork($author->top_work ?? '')
+                ->setWorkCount($author->work_count);
             });
         }
 
         return collect();
     }
 
-    private function openLibraryHttpGetRequest($path, $params) : Response {
+    /**
+     * Retrieve an author by a given key
+     *
+     * @param string $key
+     * @return array
+     */
+    public function getAuthor(string $key) : OpenLibraryAuthor {
+        $results = $this->openLibraryHttpGetRequest('authors/'.$key);
+        $result = $results->object();
+
+        if(isset($result->key)) {
+            $author = (new OpenLibraryAuthor(str_replace('/authors/','', $result->key), $result->name))
+            ->setTitle($result->title ?? '')
+            ->setBio($result->bio ?? '')
+            ->setPersonalName($result->personal_name ?? '')
+            ->setAlternateNames(isset($result->alternate_names) ? collect($result->alternate_names) : collect())
+            ->setBirthDate($result->birth_date ?? '')
+            ->setDeathDate($result->death_date ?? '')
+            ->setPeriod($result->date ?? '')
+            ->setWikipeadiaLink($result->wikipedia ?? '')
+            ->setLinks(isset($result->links) ? collect($result->links) : collect())
+            ->setPhotos(collect(isset($result->photos) ?? collect())->map(function ($photo) {
+                return 'https://covers.openlibrary.org/a/id/'.$photo.'-M.jpg';
+            }));
+
+            // Api does not provide some data returned by the search API, to include that data
+            // call search API
+            foreach($this->searchAuthors($author->getName()) as $result) {
+                if($result->getKey() == $author->getKey()) {
+                    $author->setTopSubjects($result->getTopSubjects());
+                    $author->setTopWork($result->getTopWork());
+                    $author->setWorkCount($result->getWorkCount());
+                    break;
+                }
+            }
+
+            return $author;
+        }
+
+        throw new Exception('Author not found for given key : '.$key);
+    }
+
+    private function openLibraryHttpGetRequest(string $path, array $params = []) : Response {
         $response = Http::withHeaders(['Accept' => 'application/json'])->get(self::BASE_URL.$path, $params);
 
         if($response->ok()) {
             return $response;
-        } else {
+        } 
+        else {
             throw new Exception('Open Library API request error!');
         }
     }
